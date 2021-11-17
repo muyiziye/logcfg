97c578f8a857cec953fe26901f1b3e604b07a5e3
YangLiu <liu-yang91@qq.com>
2021-11-17 14:16:24 +0800

    [feature]: update the framework to cmake

--- compile.sh
+++ compile.sh
@@ -6,4 +6,11 @@
 # Created Time: Wed 17 Nov 2021 12:51:43 PM CST
 #########################################################################
 
+# create build dir
+mkdir -pv build/{bin,relese,debug}
 
+
+pushd build/debug
+cmake ../../src
+make -j4 install
+popd 
---
+++ src/unitTest/ut_main.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: ut_main.cpp
+	> Author: LiuYang
+	> Mail: liuyang91
+	> Created Time: Wed 17 Nov 2021 02:02:20 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main(int argc, char **argv){
+    cout << "just to test" << endl;
+
+    return 0;
+}
